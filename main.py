92073163f5f5d1a8c5a7e60c0fde5a52aff53d90
Kelvin Ofori Antwi <oforik672@gmail.com>
2026-03-28 09:54:20 +0000

    learns

---
+++ main.py
@@ -0,0 +1,18 @@
+from os import name
+
+print('hello world')
+print('Joe is an idiot')
+print('I like rice,', end='')
+print('yet enjoying it with stew is the best')
+print("555", "0123", sep="-")
+
+print('Please Enter Your Name')
+name = input()
+
+print('Your name is' ,name)
+
+print('total')
+total = input()
+print('Your total is' ,total)
+
+print('They said, "try it" so I did')
---
+++ pass.py
@@ -0,0 +1,9 @@
+import pandas as pd
+pd.DataFrame
+df = pd.DataFrame
+
+df['color'] = ['blue','red', 'green', 'yellow']
+df['radius'] = ['2', '3', '4', '5']
+df #showresult
+df['diameter'] = ['radius']*2
+df #output result
